3d143d261bb2af520b78c39fd0909e168e29edf4
Jeisson <jeisson@nuevalengua.com>
2019-05-16 08:44:08 -0500

    Linea base

---
+++ src/main/java/com/sophos/poc/wsrestconsultarsaldo/WsRestConsultarSaldoApplication.java
@@ -0,0 +1,13 @@
+package com.sophos.poc.wsrestconsultarsaldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsRestConsultarSaldoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsRestConsultarSaldoApplication.class, args);
+	}
+
+}
